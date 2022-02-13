0139a342b358f7c640ace6e1123ab98b0e6007f9
rohanreddy98 <rohanreddy.venkannagari@gmail.com>
2022-02-13 11:52:17 +0530

    redux sample

--- redux-store.js
+++ redux-store.js
@@ -1,7 +1,28 @@
+//  live example -> 
+// 1. visiting booking center  
+// 2. form new booking/ cancel ticket 
+// 3. submit form 
+// 4. ticket counter 1, ticket counter 2, ticket counter 3
+// 5. ticket avaialbility, cancelled tickets, trains status 
+// 6. notification to mobile operator
+// 7. message for forwarded user  
+
+//  redux example -> 
+//  1. action creators
+//  2. action ( { type: 'booking', payload: 'info'}) 
+//  3. dispatch 
+//  4. reducers 1, reducers 2, reducers 3 
+//  5. central store 
+//  6. subscription 
+//  7. pass state as props, mapStateToProps()
+
 // action creator -> action -> dispatch -> reducers -> store
+// you -> booking form -> submit form -> ticket counters -> railway central  store
 
 //Action creators
 // person who is submitting the form
+
+// action -> it consists of two fills, 1) type, 2)payload
 
 const newBooking = (name, amount) => {
   return {
@@ -53,3 +74,21 @@ const accounting = (totalMoney=100, action) => {
 }
 
 //Redux store
+
+console.log(Redux);
+const {createStore, combineReducers} = Redux;
+
+const railwayCentralStore = combineReducers({
+  accounting : accounting,
+  reservationHistory: reservationHistory,
+  cancellationHistory: cancellationHistory
+})
+
+const store = createStore(railwayCentralStore);
+
+const action = newBooking("rohan", 20);
+
+store.dispatch(action);
+store.dispatch(cancelBooking('rohan', 10));
+console.log(store.getState());
+
